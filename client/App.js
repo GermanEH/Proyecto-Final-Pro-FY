f96953305d7f1e36233e1f3e46e11838b3d42101
juanml08 <jmlopez-08@gotmail.com>
2022-11-07 16:13:32 -0500

    componente Loading y componente Intro

--- client/App.js
+++ client/App.js
@@ -1,4 +1,6 @@
-import { HamburgerMenu} from './components/menu/HamburgerMenu'
+/* import { Loading } from './components/loading/Loading'; */
+/* import { HamburgerMenu} from './components/menu/HamburgerMenu'; */
+import { Intro } from './components/intro/Intro';
 
 /* import { store } from './store'
 import { Provider } from 'react-redux' */
@@ -8,8 +10,13 @@ import { Provider } from 'react-redux' */
 export default function App() {
   //aqui va redux 99% seguro
   return (
-      <HamburgerMenu/>
-  );
+    <>
+      {/* <HamburgerMenu/> */}
+    {/*   <Loading/> */}
+      <Intro/>
+    </>
+);
 }
+
 
 
