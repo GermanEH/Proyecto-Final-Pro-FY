a67f8804e7d0882c2254d3b282635151ae583892
German Horianski <germanezequielhorianski@gmail.com>
2022-11-08 22:55:09 -0300

    Hice el slice getPacientsSlice

--- client/src/slices/getPacients.js
+++ client/src/slices/getPacients.js
@@ -1,15 +1,45 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { axios } from 'axios'
 
 const initialState = {
-
+  pacients: []
 }
 
-const getPacients = createSlice({
-  name: second,
+export const getPacients = createAsyncThunk('pacients/getPacients', async () => {
+  try {
+      const response = await axios.get('http://localhost:3001/api/users')
+      const data = response.data.sort(function(a, b) {
+          if(a.name < b.name) return -1;
+          if(a.name > b.name) return 1;
+          return 0
+      })
+      return data
+  } catch (error) {
+      return error.message
+  }        
+})
+
+export const getPacientsSlice = createSlice({
+  name: 'getPacientsSlice',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder 
+        .addCase(getPacients.pending, (state, _action) => {
+            state.status = 'loading'
+        })
+        .addCase(getPacients.fulfilled, (state, action) => {
+            state.status = 'succeeded'
+            state.temperament = action.payload
+            return action.payload
+        })
+        .addCase(getPacients.rejected, (state, action) => {
+            state.status = 'failed'
+            state.error = action.error.message
+        })
+      }
 });
 
-export const {} = getPacients.actions
-
-export default getPacients.reducer
+export const pacients = (state) => state.pacients
+export const pacientsStatus = (state) => state.status
+export const pacientsError = (state) => state.error
