223084b4a5707972ce60d44b74cb1e30bf5da95d
German Horianski <germanezequielhorianski@gmail.com>
2022-11-09 15:54:12 -0300

    Agregué los archivos getPacientDetail y getProfessionalDetail

--- client/src/components/pacient/FormPacient.jsx
+++ client/src/components/pacient/FormPacient.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 // import { useEffect } from 'react'
 import { View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
-import { postPacient } from '../../slices/postPacientSlice'
+import { postPacient } from '../../slices/postPacient'
 
 export function FormPacient () {
-)
+
   const dispatch = useDispatch()
 
   const newPacient = {
-    name: Germánicus,
-    password: LoremIpsum,
+    first_name: Germánicus,
+    last_name: Hori,
     DNI: 30213311,
     provincia: 'Buenos Aires',
     ciudad: 'Mar del Plata',
--- client/src/components/professional/FormProfessional.jsx
+++ client/src/components/professional/FormProfessional.jsx
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+// import { useEffect } from 'react'
+import { View } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+import { postPacient } from '../../slices/postPacientSlice'
 
-export function FormProfessional () {
+export function FormPacient () {
+
+  const dispatch = useDispatch()
+
+  const newProfessional = {
+    first_name: Germánicus,
+    last_name: Hori,
+    password: LoremIpsum,
+    matricula: 1231,
+    especialidad: 'Odontologo',
+    provincia: 'Buenos Aires',
+    ciudad: 'Mar del Plata',
+    código_postal: 7600,
+    dirección: 'Siempre viva 123',
+    disponibilidad_horaria: 'Lunes a Viernes de 9 a 12 hs.',
+    modalidad_de_consulta: ['Videollamada', 'Presencial']
+  }
+
   return (
     <View>
-      <Text> </Text>
+      <Button
+                title='PostPacient'
+                onPress={() => dispatch(postPacient(newProfessional))}/>
     </View>
   )
 }
