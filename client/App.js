704d93737a894a9c167d7a6d5e22cd6f5d471a9c
juanml08 <jmlopez-08@gotmail.com>
2022-11-09 12:16:15 -0500

    Estilos Onboard y componente Logging

--- client/App.js
+++ client/App.js
@@ -8,14 +8,11 @@ import { FormProfessional } from './src/components/professional/FormProfessional
 import { HomePacient } from './src/components/pacient/HomePacient'
 import { HomeProfessional } from './src/components/professional/HomeProfessional'
 import { Consultas } from './src/components/shared/Consultas'
-import { Provider } from 'react-redux'
-import { store } from './src/store'
 /* import { Loading } from './components/loading/Loading'; */
 /* import { HamburgerMenu} from './components/menu/HamburgerMenu'; */
-import { Intro } from './components/intro/Intro';
+import { store } from './src/store';
+import { Provider } from 'react-redux'; 
 
-/* import { store } from './store'
-import { Provider } from 'react-redux' */
 
 
 const Stack = createStackNavigator()
@@ -48,17 +45,11 @@ export default function App() {
             <Stack.Screen
               name="HomeProfessional"
               component={HomeProfessional}/>
-          </Stack.Navigator>
+          </Stack.Navigator>  
         </NavigationContainer>
-       </Provider>
+      </Provider>
       // </React.StrictMode>
   );
-    <>
-      {/* <HamburgerMenu/> */}
-    {/*   <Loading/> */}
-      <Intro/>
-    </>
-);
 }
 
 
