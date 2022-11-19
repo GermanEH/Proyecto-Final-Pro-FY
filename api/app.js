a0a11d9b6c201e1a7e8983ff163cd4941f8a133c
Hans F. Trauwitz V <hans.trauwitz@gmail.com>
2022-11-19 16:46:57 -0600

    bug arregladoÂ cloudinary

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const dbConnect = require('./config/mongo')
-// const fileUpload = require("express-fileupload")
+const fileUpload = require("express-fileupload")
 
 const app = express()
 
@@ -10,13 +10,13 @@ app.use(cors())
 app.use(express.json())
 // app.use(express.json({ limit: '500mb' }));
 // app.use(express.urlencoded({ limit: '500mb' })); //!ojito
-
-// app.use(
-//   fileUpload({
-//     useTempFiles: true,
-//     tempFileDir: "./uploads",
-//   })
-// );
+//hans24
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
 
 
 app.use(express.static("storage"))
