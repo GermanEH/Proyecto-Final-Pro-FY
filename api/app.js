cadced95cdcafa2b16a54968efeea793bb3f9982
amma2win <102752491+amma2win@users.noreply.github.com>
2022-11-19 20:32:52 -0300

    acomodando forms

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 // app.use(express.json({ limit: '500mb' }));
 // app.use(express.urlencoded({ limit: '500mb' })); //!ojito
-=======
+
 
 app.use(
   fileUpload({
