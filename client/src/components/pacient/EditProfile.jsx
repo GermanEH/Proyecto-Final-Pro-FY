8c427cb7c86b4ffa7f9d2cab6f63fbd67c92f1b5
Yaileen Manjarres <yaileenmanjarres@gmail.com>
2022-11-23 09:43:55 -0500

    EditProfile listo

--- client/src/components/pacient/EditProfile.jsx
+++ client/src/components/pacient/EditProfile.jsx
@@ -4,43 +4,70 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
 } from "react-native";
 import theme from "../../theme";
 
 
 export function EditProfile() {
   return (
-    <SafeAreaView>
+    <ScrollView contentContainerStyle={{ flexGrow: 1, backgroundColor: "white" }}>
       <View style={styles.container}>
-        <Text>
-          E-mail
-        </Text>
-        <TextInput style={styles.input} placeholder='E-mail'>
-        </TextInput>
-        <Text>
-          Celular
-        </Text>
-        <TextInput style={styles.input} placeholder='Celular'>
-        </TextInput>
+        <View style={styles.changePicture}>
+          <View style={{ backgroundColor: '#ddd', height: 50, width: 50, borderRadius: 25 }}>
+          </View>
+          <View style={{ paddingLeft: 12 }}>
+            <Text style={{ paddingBottom: 4, fontWeight: theme.fontWeights.bold, fontSize: theme.fontSize.secondaryText }}>
+              Usuario
+            </Text>
+            <TouchableOpacity>
+              <Text style={{ color: theme.colors.primaryColor }}>
+                Cambiar foto de perfil
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.changeName}>
+          <Text style={{
+            fontSize: theme.fontSize.terciaryText,
+            fontWeight: theme.fontWeights.bold,
+            paddingBottom: 4
+          }}
+          >
+            Nombre
+          </Text>
+          <TextInput style={{ backgroundColor: '#ddd', height: 40, paddingLeft: 5 }}>
+
+          </TextInput>
+          <Text style={{
+            fontSize: 15,
+            color: '#aaa',
+            paddingTop: 4
+          }}
+          >
+            Nombre de usuario.
+          </Text>
+        </View>
       </View>
-    </SafeAreaView>
+    </ScrollView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginHorizontal: 25
+  },
+  changePicture: {
+    paddingTop: 15,
     display: 'flex',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  input: {
-    height: 60,
-    width: 390,
-    backgroundColor: '#ededed',
-    borderRadius: 10,
-    padding: 10,
-    marginBottom: 10,
-    fontSize: theme.fontSize.secondaryText
-  },
+  changeName: {
+    paddingTop: 12,
+  }
 });
-
