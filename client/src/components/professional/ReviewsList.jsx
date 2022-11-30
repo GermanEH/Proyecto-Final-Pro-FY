0d4622ad9e40a1eab9a915103ede48f6fbb8c949
German Horianski <germanezequielhorianski@gmail.com>
2022-11-30 18:24:38 -0300

    Retoqué ReviewsList

--- client/src/components/professional/ReviewsList.jsx
+++ client/src/components/professional/ReviewsList.jsx
@@ -25,8 +25,7 @@ const Respuesta=()=> {
 }
 
   return (
-    <View style={{ width: 350,height: 430}}>
-      <Text >        Listado de Reviews</Text>
+    <View style={{ width: 350,height: 430, marginTop: 20}}>
       <View>
          <ScrollView horizontal={true}>
 
@@ -41,7 +40,7 @@ const Respuesta=()=> {
                 height:20,
                 borderColor: "grey",
                  }}
-                placeholder="Responder Reviews"
+                placeholder="Responder"
                 onChangeText={value=>setRespuestaReview(value)}
               />
       </View>
