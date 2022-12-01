24a468a30c1a3cbf6015ce35d1ca061439d3c4f8
juanml08 <jmlopez-08@gotmail.com>
2022-12-01 01:08:06 -0500

    estilos

--- client/src/components/main/SignIn.jsx
+++ client/src/components/main/SignIn.jsx
@@ -10,36 +10,36 @@ import {
   useWindowDimensions,
   KeyboardAvoidingView,
 } from "react-native";
-import {
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import Logo from "../../assets/logo.png";
 import CustomButtom from "../CustomButton/CustomButton";
 import { auth1 } from "../../../firebase-config.js";
 
-import {useSelector, useDispatch} from 'react-redux'
-import { loggedUser } from '../../slices/pacients';
-import "expo-dev-client"
-import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
+import { useSelector, useDispatch } from "react-redux";
+import { loggedUser } from "../../slices/pacients";
+import "expo-dev-client";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
+import auth from "@react-native-firebase/auth";
 import theme from "../../theme";
-import {useForm, Controller } from 'react-hook-form';
+import { useForm, Controller } from "react-hook-form";
 
 // const provider = new GoogleAuthProvider();
 
 export function SignIn({ route, navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const {control, handleSubmit} = useForm()
-  const [initializing, setInitializing] = useState(true)
-  const [userLogged, setUserLogged] = useState(null)
-  
+  const { control, handleSubmit } = useForm();
+  const [initializing, setInitializing] = useState(true);
+  const [userLogged, setUserLogged] = useState(null);
 
-  const loggedU = useSelector((state) => state.pacients.logged)
+  const loggedU = useSelector((state) => state.pacients.logged);
 
   const { height } = useWindowDimensions();
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const handleSignIn = () => {
     signInWithEmailAndPassword(auth1, email, password)
@@ -72,43 +72,47 @@ export function SignIn({ route, navigation }) {
   };
 
   GoogleSignin.configure({
-    webClientId:'882289933965-m2vb8le0tsfhbib51bq33tmu02otvscr.apps.googleusercontent.com',
-})
+    webClientId:
+      "882289933965-m2vb8le0tsfhbib51bq33tmu02otvscr.apps.googleusercontent.com",
+  });
 
-    const onAuthStateChanged = (userL) =>{
-        setUserLogged(userL);
-        if(userL){
-            const loggedUs = {displayName: userL["displayName"], email: userL["email"], emailVerified: userL["emailVerified"]}
-            dispatch(loggedUser(loggedUs))
-            }
-        if (initializing) setInitializing(false);
+  const onAuthStateChanged = (userL) => {
+    setUserLogged(userL);
+    if (userL) {
+      const loggedUs = {
+        displayName: userL["displayName"],
+        email: userL["email"],
+        emailVerified: userL["emailVerified"],
+      };
+      dispatch(loggedUser(loggedUs));
     }
+    if (initializing) setInitializing(false);
+  };
 
-    useEffect(() => {
+  useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     // return subscriber; // unsubscribe on unmount
-    }, []);
+  }, []);
 
-    if (initializing) return null;
+  if (initializing) return null;
 
+  const onGoogleButtonPress = async () => {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
 
-    const onGoogleButtonPress = async () =>  {
-        // Check if your device supports Google Play
-        await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-        // Get the users ID token
-        const { idToken } = await GoogleSignin.signIn();
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
-        // Create a Google credential with the token
-        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    // Sign-in the user with the credential
+    auth().signInWithCredential(googleCredential);
 
-        // Sign-in the user with the credential
-        auth().signInWithCredential(googleCredential);
-        
-        route.params.usertype === "Pacient" 
-            ? navigation.navigate("HamburguerMenu", { usertype: "Pacient" }) 
-            : navigation.navigate("HamburguerMenu", { usertype: "Professional" })
-    }
-  
+    route.params.usertype === "Pacient"
+      ? navigation.navigate("HamburguerMenu", { usertype: "Pacient" })
+      : navigation.navigate("HamburguerMenu", { usertype: "Professional" });
+  };
+
   // useEffect(()=>{
   //     const unsuscribe = auth.onAuthStateChanged( user => {
   //         if(user && user.emailVerified === 'false') {alert('Correo electronico no verificado')}
@@ -143,68 +147,92 @@ export function SignIn({ route, navigation }) {
   // };
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-    {/* {(!userLogged) ? */}
-    <View style={styles.signInContainer}>
-      <Image source={Logo} style={styles.logo} />
+      {/* {(!userLogged) ? */}
+      <View style={styles.signInContainer}>
+        <Image source={Logo} style={styles.logo} />
 
-      <View style={styles.inputsButtomsContainer}>
+        <View style={styles.inputsButtomsContainer}>
+          <TextInput
+            onChangeText={(text) => setEmail(text)}
+            placeholder="Correo electrónico"
+            style={styles.input}
+          ></TextInput>
 
-        <TextInput
-          onChangeText={(text) => setEmail(text)}
-          placeholder="Correo electrónico"
-          style={styles.input}
-        ></TextInput>
+          <TextInput
+            onChangeText={(text) => setPassword(text)}
+            placeholder="Contraseña"
+            style={styles.input}
+            secureTextEntry
+          ></TextInput>
 
-        <TextInput
-          onChangeText={(text) => setPassword(text)}
-          placeholder="Contraseña"
-          style={styles.input}
-          secureTextEntry
-        ></TextInput>
-
-        <View style={{ width: "85%", paddingTop: 10 }}>
-          <CustomButtom text="Ingresar" onPress={handleSubmit(handleSignIn)} />
-        </View>
-      </View>
-        <GoogleSigninButton
-              text="Ingresar con Google" 
-              onPress={onGoogleButtonPress}
-              style={{width:'87%', marginTop:20}} /> 
-        <View style={{ paddingTop: 150, alignItems: 'center'}}>
-            <Text style={{ fontSize: theme.fontSize.terciaryText, fontWeight: theme.fontWeights.bold, color: theme.colors.textColor }}>
-              ¿No tienes una cuenta?
-        </Text>
-        <View style={styles.container}>
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate(
-               ( route.params.usertype === "Pacient") ? "SignUpPacient" : "SignUpProfessional"
-              )
-            }
-          >
-            <Text
+          <View style={{ width: "85%", paddingTop: 10 }}>
+            <TouchableOpacity
               style={{
-                fontSize: theme.fontSize.secondaryText,
-                fontWeight: theme.fontWeights.bold,
-                color: theme.colors.primaryColor,
+                backgroundColor: theme.colors.primaryColor,
+                borderRadius: 10,
               }}
+              onPress={handleSubmit(handleSignIn)}
             >
-              Registrate
-            </Text>
+              <Text
+                style={{
+                  color: "white",
+                  textAlign: "center",
+                  padding: 10,
+                }}
+              >
+                Ingresar
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
-    </View>
+        <GoogleSigninButton
+          text="Ingresar con Google"
+          onPress={onGoogleButtonPress}
+          style={{ width: "87%", marginTop: 20 }}
+        />
+        <View style={{ paddingTop: 150, alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: theme.fontSize.terciaryText,
+              fontWeight: theme.fontWeights.bold,
+              color: theme.colors.textColor,
+            }}
+          >
+            ¿No tienes una cuenta?
+          </Text>
+          <View style={styles.container}>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate(
+                  route.params.usertype === "Pacient"
+                    ? "SignUpPacient"
+                    : "SignUpProfessional"
+                )
+              }
+            >
+              <Text
+                style={{
+                  fontSize: theme.fontSize.secondaryText,
+                  fontWeight: theme.fontWeights.bold,
+                  color: theme.colors.primaryColor,
+                }}
+              >
+                Registrate
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
       {/* // : */}
-       {/* navigation.navigate("HamburguerMenu", { usertype: (route.params.usertype === "pacient") ? "pacient" : "professional" }) */}
+      {/* navigation.navigate("HamburguerMenu", { usertype: (route.params.usertype === "pacient") ? "pacient" : "professional" }) */}
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   signInContainer: {
     // flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     paddingTop: 40,
     backgroundColor: "white",
   },
--- client/src/components/professional/ReviewsList.jsx
+++ client/src/components/professional/ReviewsList.jsx
@@ -1,49 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { View, Text, TouchableOpacity, ScrollView,TextInput } from 'react-native'
-import { getReviews } from '../../slices/reviewsActions'
-import { CardReviews } from './CardReviews'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import { getReviews } from "../../slices/reviewsActions";
+import { CardReviews } from "./CardReviews";
 // import { theme } from '../../../theme'
 
 export function ReviewsList() {
+  const reviews = useSelector((state) => state.reviews);
 
-  
+  const dispatch = useDispatch();
 
-  const reviews = useSelector(state => state.reviews)
- 
-  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getReviews());
+  }, []);
 
-  useEffect(() => {dispatch(getReviews())}, [])
+  const [respuestaReview, setRespuestaReview] = useState("");
 
-  const [respuestaReview, setRespuestaReview] = useState("")
-
-const Respuesta=()=> {
-
-   setRespuestaReview("")
+  const Respuesta = () => {
+    setRespuestaReview("");
     return respuestaReview;
-
-}
+  };
 
   return (
-    <View style={{ width: 350,height: 430, marginTop: 20}}>
-      <View>
-         <ScrollView horizontal={true}>
-
-             {
-                reviews.reviews.map( (p,i)=> <CardReviews id={i+1 } review={p.review} CambioRespuesta={Respuesta} key={i} /> )
-        }
-         </ScrollView>
-        <TextInput
-                value={respuestaReview}
-                style={{
-                width: "70%",
-                height:20,
-                borderColor: "grey",
-                 }}
-                placeholder="Responder"
-                onChangeText={value=>setRespuestaReview(value)}
+    <View
+      style={{
+        width: 350,
+        height: 300,
+        marginTop: 20,
+        backgroundColor: "white",
+      }}
+    >
+      <View style={{ paddingBottom: 20 }}>
+        <ScrollView horizontal={true}>
+          <View style={{ flexDirection: "row" }}>
+            {reviews.reviews.map((p, i) => (
+              <CardReviews
+                id={i + 1}
+                review={p.review}
+                CambioRespuesta={Respuesta}
+                key={i}
               />
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+      <View style={{ paddingTop: 20 }}>
+        <TextInput
+          value={respuestaReview}
+          style={{
+            width: "70%",
+            height: 20,
+            backgroundColor: "#BCBCBC",
+            height: 40,
+            borderRadius: 10,
+          }}
+          placeholder="Coloque su respuesta aqui"
+          onChangeText={(value) => setRespuestaReview(value)}
+        />
       </View>
     </View>
-  )
+  );
 }
